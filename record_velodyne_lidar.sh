8da76c8d3669e5e3413f08c44a75345c69d9d066
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-04-19 11:45:08 +0900

    [Perf]: Replace rosbag fancy to rosbag

--- record_velodyne_lidar.sh
+++ record_velodyne_lidar.sh
@@ -3,6 +3,6 @@
 # Create the directory if it doesn't exist
 mkdir -p ~/Downloads/$2
 
-rosbag_fancy record --lz4 -o ~/Downloads/$2/$1 \
+rosbag record --lz4 -o ~/Downloads/$2/$1.bag \
   /$3/velodyne_packets \
   /tf_static
