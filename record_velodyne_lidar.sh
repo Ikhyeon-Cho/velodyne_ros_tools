e2bbffb3de6f3f68ddbb6c4b061021b1e4a2b7a8
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-04-27 21:51:31 +0900

    [Refactor]: Reorganize the launch files and scripts

--- record_velodyne_lidar.sh
+++ record_velodyne_lidar.sh
@@ -3,6 +3,6 @@
 # Create the directory if it doesn't exist
 mkdir -p ~/Downloads/$2
 
-rosbag record --lz4 -o ~/Downloads/$2/$1.bag \
-  /$3/velodyne_packets \
-  /tf_static
+rosbag record -O ~/Downloads/$2/$1 \
+  /tf_static \
+  /$3/velodyne_packets
